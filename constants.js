8c201b15089bbc70492a6278203428a21fd863d1
GenaroLoprete <40218987+GenaroLoprete@users.noreply.github.com>
2021-01-03 19:56:27 -0300

    Const pasan al env

--- constants.js
+++ constants.js
@@ -1,1 +1,4 @@
-module.exports.maxNumber = 5;
+module.exports.maxNumber = ENV.MAXNUMBER;
+module.exports.username = ENV.USERNAME;
+module.exports.password = ENV.PASSWORD;
+module.exports.channels = ENV.CHANNELS;
